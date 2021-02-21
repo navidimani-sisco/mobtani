0637443e7400070eeebc925488994b8bfc984363
navidimani-sisco <52500755+navidimani-sisco@users.noreply.github.com>
2021-02-22 01:10:13 +0330

    Add files via upload
    Uploading initial project files

--- settings.php
+++ settings.php
@@ -1,1 +1,14 @@
+<?php
+if( ! defined('DBHOST') )
+	define( 'DBHOST', 'localhost' );
+if( ! defined('DBUSER') )
+	define( 'DBUSER', 'root' );
+if( ! defined('DBPASS') )
+	define( 'DBPASS', '' );
+if( ! defined('DBNAME') )
+	define( 'DBNAME', 'mobtani' );
 
+$dbHost =	DBHOST;
+$dbUser =	DBUSER;
+$dbPass =	DBPASS;
+$dbname =	DBNAME;
