b1062fe037cf3c2163c9986b7df26a43726e8072
navid imani <52500755+navidimani-sisco@users.noreply.github.com>
2021-05-17 00:35:43 +0430

    Add files via upload

--- public/addUser.php
+++ public/addUser.php
@@ -1,39 +1,42 @@
 <?php
 	include '__php__.php';
-	
-	if( Authentication :: check() ){		
-		Alert::alerts('قبلا وارد شدید!', 'warning');
-		redirect('dashboard.php');
-	}
-	
+		
 	$parameters = $_POST;
 	if( isset( $_POST['submit'] ) ){ // اگر از فرم آمده
-		$db = new DB();
-		
-		$where = "email = '{$_POST['email']}'";
-		$table = User::find( $where );
-		if( ! isset($table[0]) ) {// username تکراری نباشد
-			// کاربر را ثبت نام کن
-			unset( $parameters['submit'] );
-			$parameters['passwordHash'] = password_hash($parameters['password'], PASSWORD_DEFAULT);
-			unset( $parameters['password'] );
-			$uid = User::add( $parameters );
+		if( Validation :: tokenCheck( $_POST['csrf_token'] ) )
+		{
+			$db = new DB();
 			
-			// کاربر را لاگین هم کن
-			Authentication :: login( $uid );
-			
-			Alert::alerts("{$parameters['firstname']} {$parameters['lastname']} خوش آمدی!", 'success');
-			
-			if( isset($_SESSION['redirect']) ){
-				$redirectAddress = $_SESSION['redirect'];
-				unset( $_SESSION['redirect'] );
-				redirect( $redirectAddress );
+			$where = "email = '{$_POST['email']}'";
+			$table = User::find( $where );
+			if( ! isset($table[0]) ) {// username تکراری نباشد
+				// کاربر را ثبت نام کن
+				unset( $parameters['submit'] );
+				unset( $parameters['csrf_token'] );
+				$parameters['passwordHash'] = password_hash($parameters['password'], PASSWORD_DEFAULT);
+				unset( $parameters['password'] );
+				$uid = User::add( $parameters );
+				
+				// کاربر را لاگین هم کن
+				Authentication :: login( $uid );
+				
+				Alert::alerts("{$parameters['firstname']} {$parameters['lastname']} خوش آمدی!", 'success');
+				
+				if( isset($_SESSION['redirect']) ){
+					$redirectAddress = $_SESSION['redirect'];
+					unset( $_SESSION['redirect'] );
+					redirect( $redirectAddress );
+				}
+				else
+					redirect('dashboard.php');
+				}
+			else{ // اگر ایمیل تکراری باشد
+				Alert::alerts('این ایمیل از قبل وجود دارد!');
 			}
-			else
-				redirect('dashboard.php');
-			}
-		else{ // اگر ایمیل تکراری باشد
-			Alert::alerts('این ایمیل از قبل وجود دارد!');
+		}
+		else{
+			Alert::alerts('دسترسی غیر مجاز!');
+			redirect('dashboard.php');
 		}
 		unset( $db );
 		
--- public/editUser.php
+++ public/editUser.php
@@ -1,32 +1,37 @@
 <?php
-	include '../includes/settings.php';
-	include '../includes/functions.php';
-		
-	if( isset( $_GET['id'] ) ) {
-		
-		
-		if( isset( $_POST['submit'] ) ){
-			
-			$parameters = $_POST;
-			unset( $parameters['submit'] );
-			$parameters['id'] = $_GET['id'];
-			$db = new DB();
-			$table = User::update( $parameters );
-			unset( $db );			
-		}
-		
-		$db = new DB();
-		$table = User::find("id = {$_GET['id']}");
-		$row = $table[0];
-		
-		unset( $db );
-		
-	}
-	else{
-		alerts('شناسه کاربر نامشخص!');
-		//redirect
-	}
+
+include '__php__.php';
+
+$db = new DB();
+if( ! Authorization :: check('UserEdit') ){		
+	Alert::alerts('شما مجوز ویرایش حساب را ندارید!');
+	redirect('dashboard.php');
+}
 	
-	$alerts = alerts();
-	echo $alerts;
-	include '../includes/views/editUser.php';
+if( isset( $_GET['id'] ) ) {
+	
+	// validate id
+	$pageID = $_GET['id'];
+	if( isset( $_POST['submit'] ) ){ // اطلاعات جدید
+		// ویرایش کن
+		//validate POST[]
+		$parameters = $_POST;
+		unset( $parameters['submit'] );
+		
+		$parameters['id'] = $pageID;
+		
+		$table = User::update( $parameters );			
+	}
+	// اطلاعات کاربر را از دیتابیس دریافت کن
+	$table = User::find("id = {$pageID}");
+	$row = $table[0];		
+}
+else{
+	Alert::alerts('شناسه کاربر نامشخص!');
+	//redirect
+}
+
+unset( $db );
+
+$alerts = Alert::alerts();
+view('editUser', null, $row , $alerts);
